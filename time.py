70543bf90b94835deafc84318da532a0acff730e
Raviraj Walke <ravirajwalke9@gmail.com>
2018-01-09 18:51:37 +0530

    search engine complete

---
+++ time.py
@@ -0,0 +1,11 @@
+import time
+def time_execution(code):
+    start=time.clock()
+    result=eval(code)
+    run_time=time.clock()-Start
+    return result,run_time
+
+def spin_loop(n):
+    i=0
+    while i<n:
+        i=i+1
