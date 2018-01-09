39d68b9287da43adaf0376d012905db101470d40
Raviraj Walke <ravirajwalke9@gmail.com>
2018-01-09 18:17:12 +0530

    basic crawler

---
+++ add_to_index.py
@@ -0,0 +1,21 @@
+
+index = []
+
+def add_to_index(index,keyword,url):
+    for entry in index:
+        if(entry[0]==keyword):
+            if(url not in entry[1]):
+                entry[1].append(url)
+                return
+        index.append([keyword,[url]])
+
+
+
+
+
+add_to_index(index,'udacity','http://udacity.com')
+add_to_index(index,'computing','http://acm.org')
+add_to_index(index,'udacity','http://npr.org')
+print index
+#>>> [['udacity', ['http://udacity.com', 'http://npr.org']],
+#>>> ['computing', ['http://acm.org']]]
